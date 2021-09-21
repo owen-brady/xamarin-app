b5eb987c6c28412c465102ddc3e02a654d6a11cc
Owen Brady <owen.brady@mm-bradyo.peace.daveramsey.com>
2021-09-21 08:09:22 -0500

    Stock Xamarin Flyout app

---
+++ MyFirstXamarinApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MyFirstXamarinApp.Services;
+using MyFirstXamarinApp.Views;
+
+namespace MyFirstXamarinApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
